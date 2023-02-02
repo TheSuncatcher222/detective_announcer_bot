eee7e0e7a6c54f47891d0f2aa1f0a797d9d85599
Kirill Svidunovich <Altair21817@gmail.com>
2023-02-03 01:21:21 +0300

    Reveal detectit vk id

--- app_data.py
+++ app_data.py
@@ -7,5 +7,5 @@ TELEGRAM_BOT_TOKEN: str = os.getenv('TELEGRAM_BOT_TOKEN')
 VK_TOKEN: str = os.getenv('VK_TOKEN')
 VK_TOKEN_ADMIN: str = os.getenv('VK_ADMIN_TOKEN')
 VK_USER_ME: str = os.getenv('VK_USER_ME')
-VK_CHAT_TARGET: int = os.getenv('VK_CHAT_TARGET')
-VK_GROUP_TARGET: int = os.getenv('VK_GROUP_TARGET')
+VK_CHAT_TARGET: int = 40914100
+VK_GROUP_TARGET: int = 40914100
