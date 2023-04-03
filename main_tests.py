0b44d76538343c706400d4d3a1ea6a0eb93d49cf
Kirill Svidunovich <Altair21817@gmail.com>
2023-04-03 16:02:30 +0300

    Add test_define_post_topic

--- main_tests.py
+++ main_tests.py
@@ -1,8 +1,9 @@
-from tests import test_main
+from tests import test_main, test_app_vk
 
 
 def main_tests():
     test_main.test_json_data_read_write()
+    test_app_vk.test_define_post_topic()
 
 
 if __name__ == '__main__':
--- tests/test_app_vk.py
+++ tests/test_app_vk.py
@@ -2,3 +2,29 @@
 # Вводить некорректный тип данных
 # Вводить невалидные данные
 # Вводить неверное количество данных
+
+from tests.vk_wall_examples import (
+    EXAMPLE_CHECKIN, EXAMPLE_OTHER, EXAMPLE_PRIZE_RESULTS, EXAMPLE_PREVIEW,
+    EXAMPLE_RATING, EXAMPLE_RESULTS, EXAMPLE_TEAMS)
+
+from project.app_vk import define_post_topic
+
+
+def test_define_post_topic():
+    post: dict = EXAMPLE_CHECKIN
+    assert define_post_topic(post) == 'checkin'
+    post = EXAMPLE_OTHER
+    assert define_post_topic(post) == 'other'
+    post = EXAMPLE_PRIZE_RESULTS
+    assert define_post_topic(post) == 'prize_results'
+    post = EXAMPLE_PREVIEW
+    assert define_post_topic(post) == 'preview'
+    post = EXAMPLE_RATING
+    assert define_post_topic(post) == 'rating'
+    post = EXAMPLE_RESULTS
+    assert define_post_topic(post) == 'results'
+    post = EXAMPLE_TEAMS
+    assert define_post_topic(post) == 'teams'
+
+    print('test_json_data_read_write PASSED')
+    return
