e5b5b15757cfd9835c1ce4e0a7c9be0a1989b8e7
Kirill Svidunovich <Altair21817@gmail.com>
2023-04-03 19:05:00 +0300

    Add test_game_dates_add_weekday_place

--- main_tests.py
+++ main_tests.py
@@ -4,8 +4,8 @@ from tests import test_main, test_app_vk
 def main_tests():
     test_main.test_json_data_read_write()
     test_app_vk.test_define_post_topic()
+    test_app_vk.test_game_dates_add_weekday_place()
 
 
 if __name__ == '__main__':
     main_tests()
-    
--- tests/test_app_vk.py
+++ tests/test_app_vk.py
@@ -7,11 +7,11 @@ from tests.vk_wall_examples import (
     EXAMPLE_CHECKIN, EXAMPLE_OTHER, EXAMPLE_PRIZE_RESULTS, EXAMPLE_PREVIEW,
     EXAMPLE_RATING, EXAMPLE_RESULTS, EXAMPLE_TEAMS)
 
-from project.app_vk import define_post_topic
+from project.app_vk import define_post_topic, game_dates_add_weekday_place
 
 
 def test_define_post_topic():
-    post: dict = EXAMPLE_CHECKIN
+    post: dict[str] = EXAMPLE_CHECKIN
     assert define_post_topic(post) == 'checkin'
     post = EXAMPLE_OTHER
     assert define_post_topic(post) == 'other'
@@ -27,4 +27,28 @@ def test_define_post_topic():
     assert define_post_topic(post) == 'teams'
 
     print('test_json_data_read_write PASSED')
+    return
+
+
+def test_game_dates_add_weekday_place():
+    NL = '\n'
+    GAME_DATES_INPUT: list[str] = [
+        '17 марта, 19:00 — секретное место на Чернышевской',
+        '21 апреля, 19:00 — секретное место на Горьковской',
+        '23 марта, 19:00 — секретное место на Василеостровской',
+        '31 декабря, 23:59 — секретное место в нигде',
+        '01 января, 00:00 — ']
+    # Results are valid until December 31th 2023 23:59!
+    GAME_DATES_OUTPUT: list[str] = [
+        '17 марта (вс), 19:00 — Дворец «Олимпия» (Литейный пр., д. 14, ст.м. Чернышевская)',
+        '21 апреля (пт), 19:00 — ParkKing (Александровский Парк, 4, ст.м. Горьковская)',
+        '23 марта (сб), 19:00 — Цинь (16-я лин. B.O., 83, ст.м. Василеостровская)',
+        '31 декабря (вс), 23:59 — секретное место в нигде',
+        '01 января (пн), 00:00 — ']
+    date_format = game_dates_add_weekday_place(GAME_DATES_INPUT)
+    for date in range(len(GAME_DATES_INPUT)):
+        assert date_format[date] == GAME_DATES_OUTPUT[date], (
+            f'Format FAILED!{NL}INPUT: {GAME_DATES_INPUT[date]}{NL}'
+            f'FORMAT: {date_format[date]}{NL}CORRECT: {GAME_DATES_OUTPUT[date]}')
+    print('test_game_dates_add_weekday_place PASSED')
     return
