e9753ea50c6892fccd196ed1e92deff12d52f01d
Kirill Svidunovich <Altair21817@gmail.com>
2023-04-06 16:50:00 +0300

    Add test_form_game_dates_text

--- main_tests.py
+++ main_tests.py
@@ -21,6 +21,7 @@ def main_tests():
             f'   - test_parse_post_preview {YELLOW_SKIPPED}{NL}'
             f'   - test_parse_post_stop_list {YELLOW_SKIPPED}{NL}'
             f'   - test_parse_post {YELLOW_SKIPPED}{NL}')
+    test_app_telegram.test_form_game_dates_text()
     test_app_telegram.test_rebuild_team_config()
 
 
--- tests/test_app_telegram.py
+++ tests/test_app_telegram.py
@@ -1,8 +1,59 @@
-from project.app_telegram import rebuild_team_config
+from project.app_telegram import form_game_dates_text, rebuild_team_config
 
 from project.data.app_data import TEAM_CONFIG
 
 from tests.test_main import GAP, GAP_DASH, GREEN_PASSED, NL, RED_FAILED
+
+
+def test_form_game_dates_text():
+    game_dates = {
+        0: {
+                'date_location': 'Не смогу быть',
+                'teammates_count': 0,
+                'teammates': {}},
+        1: {
+                'date_location': 'Какая-та локация №1',
+                'teammates_count': 4,
+                'teammates': {
+                    'user_1': 2,
+                    'user_2': 1,
+                    'user_3': 1}},
+        2: {
+                'date_location': 'Другая локация в 18:00',
+                'teammates_count': 5,
+                'teammates': {
+                    'user_1': 4,
+                    'user_10': 1}}}
+    result = form_game_dates_text(game_dates)
+    expected: str = (
+        '————————————\n'
+        '1️⃣ Какая-та локация №1 | 4\n'
+        '————————————\n'
+        'user_1: 2\n'
+        'user_2: 1\n'
+        'user_3: 1\n'
+        '————————————\n'
+        '2️⃣ Другая локация в 18:00 | 5\n'
+        '————————————\n'
+        'user_1: 4\n'
+        'user_10: 1\n'
+        '————————————\n'
+        '🚫 Не смогу быть | 0\n'
+        '————————————')
+    errors = []
+    try:
+        assert result == expected
+    except AssertionError:
+        errors.append((result, expected))
+    if not errors:
+        print(f'test_form_game_dates_text {GREEN_PASSED}')
+    else:
+        print(f'test_form_game_dates_text {RED_FAILED}')
+        for result, expected in errors:
+            print(
+                f"Expected: {NL}{expected}{NL}"
+                f"Got: {NL}{result}")
+    return
 
 
 def test_rebuild_team_config():
@@ -38,9 +89,9 @@ def test_rebuild_team_config():
         except AssertionError:
             errors.append((num, result, expected))
     if not errors:
-        print(f'test_define_post_topic {GREEN_PASSED}')
+        print(f'test_rebuild_team_config {GREEN_PASSED}')
     else:
-        print(f'test_define_post_topic {RED_FAILED}')
+        print(f'test_rebuild_team_config {RED_FAILED}')
         for num, result, expected in errors:
             print(
                 f"{GAP_DASH}For {num} position in game_dates:{NL}"
