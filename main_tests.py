e67e32b8288bc0a267c7834839826f9a7ecfecce
Kirill Svidunovich <Altair21817@gmail.com>
2023-04-06 20:03:20 +0300

    Sort tests by name

--- main_tests.py
+++ main_tests.py
@@ -4,6 +4,10 @@ from tests.test_main import NL, YELLOW_SKIPPED
 
 def main_tests():
     test_main.test_json_data_read_write()
+
+    test_app_telegram.test_form_game_dates_text()
+    test_app_telegram.test_rebuild_team_config_game_dates()
+    
     test_app_vk.test_define_post_topic()
     test_app_vk.test_game_dates_add_weekday_place()
     test_app_vk.test_get_post_image_url()
@@ -21,8 +25,6 @@ def main_tests():
             f'   - test_parse_post_preview {YELLOW_SKIPPED}{NL}'
             f'   - test_parse_post_stop_list {YELLOW_SKIPPED}{NL}'
             f'   - test_parse_post {YELLOW_SKIPPED}{NL}')
-    test_app_telegram.test_form_game_dates_text()
-    test_app_telegram.test_rebuild_team_config_game_dates()
 
 
 if __name__ == '__main__':
