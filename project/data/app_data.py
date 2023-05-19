e5f46e096931bfb20b8a83ab29d4f44cb217bd84
Kirill Svidunovich <Altair21817@gmail.com>
2023-05-19 12:03:34 +0300

    Add A/D tags, form JSON settings group

--- project/data/app_data.py
+++ project/data/app_data.py
@@ -19,40 +19,26 @@ VK_USER: str = os.getenv('VK_USER')
 API_TELEGRAM_UPDATE_SEC: int = 1
 API_VK_UPDATE_SEC: int = 60
 
-TEAM_CONFIG_DEFAULT: dict[str, any] = {
-    'pinned_telegram_message_id': None,
-    'game_dates': {}}
-
-DATA_FOLDER: str = 'project/data/'
-SAVED_DATA_JSON_NAME: str = 'saved_data.json'
-SAVED_DATA_JSON_DEFAULT: dict[str, int | dict[str, any]] = {
-    'last_vk_message_id_alibi': 0,
-    'last_vk_message_id_detectit': 0,
-    'last_vk_wall_id_alibi': 0,
-    'last_vk_wall_id_detectit': 0,
-    'pinned_vk_message_id_alibi': 0,
-    'pinned_vk_message_id_detectit': 0,
-    'team_config_alibi': TEAM_CONFIG_DEFAULT,
-    'team_config_detectit': TEAM_CONFIG_DEFAULT}
-
 LAST_API_ERR_DEL_SEC: int = 60 * 60
 
 """Groups main info data."""
 
-ALIBI: str = '🟣 Alibi\n'
+ALIBI: str = 'Alibi'
 ALIBI_GROUP_ID: int = 40914100
 ALIBI_GROUP_LOGO: str = (
     'https://sun9-46.userapi.com/impg/LiT08C2tWC-QeeYRDjHqaHRFyXNOYyhxFacXQA/'
     'JpfUXhL2n2s.jpg?size=674x781&quality=95&sign='
     'e8310f98da4ff095adb5e46ba20eef2d&type=album')
 ALIBI_POST_LINK: str = 'https://vk.com/alibigames?w=wall-'
-DETECTIT: str = '⚫️ Detectit\n'
+ALIBI_TAG: str = '🟣 Alibi\n'
+DETECTIT: str = 'Detectit'
 DETECTIT_GROUP_ID: int = 219311078
 DETECTIT_GROUP_LOGO: str = (
     'https://sun9-40.userapi.com/impg/frYTaWRpxfjOS8eVZayKsugTQILb9MM0uYggNQ/'
     'UhQlYUWdBh0.jpg?size=800x768&quality=95&sign='
     'bb10ce9b1e4f2328a2382faba0981c2c&type=album')
 DETECTIT_POST_LINK: str = 'https://vk.com/detectitspb?w=wall-'
+DETECTIT_TAG: str = '⚫️ Detectit\n'
 
 PINNED_POST_ORDER: int = 0
 NON_PINNED_POST_ORDER: int = 1
@@ -170,3 +156,21 @@ TEAM_REGISTER_TEXT: str = (
     'возможно по цене:\n'
     '· 500 ₽ с человека — до дня игры,\n'
     '· 600 ₽ с человека — в день игры.')
+
+"""JSON settings."""
+
+TEAM_CONFIG_DEFAULT: dict[str, any] = {
+    'pinned_telegram_message_id': None,
+    'game_dates': {}}
+
+DATA_FOLDER: str = 'project/data/'
+SAVED_DATA_JSON_NAME: str = 'saved_data.json'
+SAVED_DATA_JSON_DEFAULT: dict[str, int | dict[str, any]] = {
+    'last_vk_message_id_alibi': 0,
+    'last_vk_message_id_detectit': 0,
+    'last_vk_wall_id_alibi': 0,
+    'last_vk_wall_id_detectit': 0,
+    'pinned_vk_message_id_alibi': 0,
+    'pinned_vk_message_id_detectit': 0,
+    'team_config_alibi': TEAM_CONFIG_DEFAULT,
+    'team_config_detectit': TEAM_CONFIG_DEFAULT}
